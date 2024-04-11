21aa2230f2c4dde3427ab6183a1dbcffddcbf82a
Amir Khaledian <amir.khaledian9@gmail.com>
2024-04-11 10:47:30 +0330

    change angle in menu

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ for (i = 0; i < dropdown.length; i++) {
   dropdown[i].addEventListener("click", function () {
     this.classList.toggle("active");
     var dropdownContent = this.nextElementSibling;
+    let children = this.querySelectorAll('.fa-angle-left');
+    
     if (dropdownContent.style.display === "block") {
       dropdownContent.style.display = "none";
+      this.querySelectorAll('.fa-angle-down')[0].className="fa fa-angle-left";
     } else {
       dropdownContent.style.display = "block";
+      this.querySelectorAll('.fa-angle-left')[0].className="fa fa-angle-down";
     }
   });
 }
